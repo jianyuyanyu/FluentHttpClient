1306f7aa728a76223bd9ec51a407d06d5b45a055
F2 <916098+F2@users.noreply.github.com>
2024-07-07 14:19:29 +0200

    Maybe this is a cleaner fix

--- Client/FluentClientExtensions.cs
+++ Client/FluentClientExtensions.cs
@@ -350,7 +350,7 @@ namespace Pathoschild.Http.Client
             // ignore if empty or already absolute
             if (string.IsNullOrWhiteSpace(resource))
                 return baseUrl;
-            if (Uri.TryCreate(resource, UriKind.Absolute, out Uri? absoluteUrl) && (absoluteUrl.Scheme == "http" || absoluteUrl.Scheme == "https"))
+            if (Uri.TryCreate(resource, UriKind.Absolute, out Uri? absoluteUrl) && !absoluteUrl.IsFile)
                 return absoluteUrl;
 
             // can't combine if no base URL
